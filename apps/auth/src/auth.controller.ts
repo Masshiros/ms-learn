5bcf0066380c4da7438b542f0473a529fab485c0
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-10-30 12:19:38 +0700

    Feat(auth): Implement auth guard and auth strategy

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './users/models/user.schema';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Response } from 'express';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.authService.login(user, response);
+    response.send(user);
+  }
   @Get()
   getHello(): string {
     return this.authService.getHello();
