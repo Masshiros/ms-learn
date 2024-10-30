068c2cbff6d7c3453d52c1f9db8fcfaed619a88a
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-10-30 10:40:12 +0700

    Feat(auth): Download necessary package

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,6 +4,8 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { Reservation, ReservationSchema } from './models/reservations.schema';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -12,6 +14,13 @@ import { Reservation, ReservationSchema } from './models/reservations.schema';
       { name: Reservation.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
